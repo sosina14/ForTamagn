01a56d26d82d7a0fd52074240473efd0dcd9e37b
Sosina Ayele <pittercakity>
2024-10-05 12:56:27 +0300

    error handiling file

---
+++ robust_division_calculator.py
@@ -0,0 +1,11 @@
+def safe_divide(numerator, denominator):
+    try:
+        numerator = float(numerator)
+        denominator = float(denominator)
+        result = numerator/ denominator
+        print (f"The result of the division is {result}")
+    except ZeroDivisionError:
+        print ("Error: Cannot divide by zero.")
+    else:
+        print("Error: Please enter numeric values only")
+    
